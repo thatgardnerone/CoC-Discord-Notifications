2ca6f43ccd570c34f79b6b570964966563f3583e
Jamie Gardner <jamie@tgo.dev>
2023-06-02 14:28:32 +0000

    Basic commands to ping and display user or server info

--- app/commands/ping.js
+++ app/commands/ping.js
@@ -1,7 +1,10 @@
-export default {
-    name: 'ping',
-    description: 'Simple ping-pong health check',
-    execute(message, args) {
-        message.channel.send('pong!');
-    },
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Simple ping-pong health check'),
+    async execute(interaction) {
+        await interaction.reply('pong!');
+    }
 };
